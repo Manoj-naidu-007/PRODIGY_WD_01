19ace29901e170596a062a83fb9f50d867c9e3cd
Manoj naidu <manojmanojnaidu007@gmail.com>
2024-09-06 10:55:40 +0530

    Add files via upload

---
+++ response landing.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.getElementById('navbar');
+    function onScroll() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    }
+
+    window.addEventListener('scroll', onScroll);
+    onScroll();
+});
